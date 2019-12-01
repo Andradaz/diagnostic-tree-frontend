b235de1145ece82a30b9dabf8f7bb719e352de39
Andrada Denisa <Andradaz>
2019-12-01 22:02:13 +0200

    Navbar Routing

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -5,9 +5,6 @@ import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
-import {
-    Link
-  } from "react-router-dom"
 
 const useStyles = makeStyles(theme => ({
     paper: {
